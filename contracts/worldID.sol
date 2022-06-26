259a2f69d1cb91af5dd65b503a2a9f2843952489
Tal Derei <Tad222@lehigh.edu>
2022-06-25 23:39:27 -0400

    added voting

--- contracts/worldID.sol
+++ contracts/worldID.sol
@@ -4,67 +4,54 @@ pragma solidity ^0.8.11;
 import { IWorldID } from '@worldcoin/world-id-contracts/src/interfaces/IWorldID.sol';
 import { ByteHasher } from '@worldcoin/world-id-contracts/src/libraries/ByteHasher.sol';
 
-contract worldID {
+contract Contract {
     using ByteHasher for bytes;
 
     // Thrown when attempting to reuse a nullifier
     error InvalidNullifier();
 
-    // Emitted when an user voted successfully
-    event Vote(address receiver);
-
-    // WorldID instance that will be used for managing groups and verifying proofs
+    // The WorldID instance that will be used for verifying proofs
     IWorldID internal immutable worldId;
 
-    // WorldID group whose participants can claim this airdrop
-    uint256 internal immutable groupId; 
+    // The WorldID group ID (1)
+    uint256 internal immutable groupId = 1;
 
     // Whether a nullifier hash has been used already. Used to prevent double-signaling
     mapping(uint256 => bool) internal nullifierHashes;
 
-    // The address that holds the tokens that are being airdropped
-    /// Make sure the holder has approved voting for this contract!
-    address public immutable holder;
-
-    /// @notice Deploys a WorldIDAirdrop instance
-    /// @param _worldId The WorldID instance that will manage groups and verify proofs
-    /// @param _groupId The ID of the Semaphore group World ID is using (`1`)
-    /// @param _votes The number of votes a user has corresponding to the number of tokens they recieved 
-    /// @param _holder The address holding the tokens that will be airdropped
-    constructor(
-        IWorldID _worldId,
-        uint256 _groupId,
-        uint256 _votes,
-        address _holder
-    ) {
+    // _worldId The WorldID instance that will verify the proofs
+    constructor(IWorldID _worldId) {
         worldId = _worldId;
-        groupId = _groupId;
-        _votes = _votes;
-        holder = _holder;
     }
 
-    /// @notice Vote on a poll
-    /// @param receiver The address that is voting
-    /// @param nullifierHash The nullifier for this proof, preventing double signaling
-    /// @param proof The zero knowledge proof that demostrates the claimer has been onboarded to World ID
-    function vote(
-        address receiver,
-        uint256 nullifierHash,
+    /// @param input User's input, used as the signal. Could be something else! (see README)
+    /// @param root The of the Merkle tree, returned by the SDK.
+    /// @param nullifierHash The nullifier for this proof, preventing double signaling, returned by the SDK.
+    /// @param proof The zero knowledge proof that demostrates the claimer is registered with World ID, returned by the SDK.
+    /// @dev Feel free to rename this method however you want! We've used `claim`, `verify` or `execute` in the past.
+    function verifyAndExecute(
+        address input,
         uint256 root,
+        uint256 nullifierHash,
         uint256[8] calldata proof
     ) public {
+        // first, we make sure this person hasn't done this before
         if (nullifierHashes[nullifierHash]) revert InvalidNullifier();
+
+        // then, we verify they're registered with WorldID, and the input they've provided is correct
         worldId.verifyProof(
             root,
             groupId,
-            abi.encodePacked(receiver).hashToField(),
+            abi.encodePacked(input).hashToField(),
             nullifierHash,
             abi.encodePacked(address(this)).hashToField(),
             proof
         );
 
+        // finally, we record they've done this, so they can't do it again (proof of uniqueness)
         nullifierHashes[nullifierHash] = true;
 
-        // vote function
+        // vote function call
+        
     }
 }
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,7 +3,7 @@ import { ethers } from 'hardhat'
 async function main() {
     // Instance of WorldID contract on the mumbai testnet
     const worldIDAddress = "0xABB70f7F39035586Da57B3c8136035f87AC0d2Aa";
-    const ContractFactory = await ethers.getContractFactory('worldID')
+    const ContractFactory = await ethers.getContractFactory('Contract')
     const contract = await ContractFactory.deploy(worldIDAddress)
     await contract.deployed()
     console.log('Contract deployed to:', contract.address)
